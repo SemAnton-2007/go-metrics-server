1c21bf6b5977e5a42bf1bec81d58d09a2eb3f594
SemAnton-2007 <anton@semenov.su>
2025-03-21 20:09:43 +0300

    Инкремент 6: добавил логирование

--- cmd/server/webservers/server.go
+++ cmd/server/webservers/server.go
@@ -3,14 +3,22 @@ package webservers
 import (
 	"go-metrics-server/cmd/server/config"
 	"go-metrics-server/cmd/server/handlers"
+	"go-metrics-server/cmd/server/middleware"
 	"go-metrics-server/cmd/server/storage"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/rs/zerolog"
 )
 
 func NewServer(cfg *config.Config, storage storage.MemStorage) *http.Server {
 	r := chi.NewRouter()
+
+	// Инициализация логгера
+	logger := zerolog.New(zerolog.NewConsoleWriter()).With().Timestamp().Logger()
+
+	// Добавляем middleware для логирования
+	r.Use(middleware.LoggerMiddleware(logger))
 
 	// Регистрируем обработчики
 	r.Post("/update/{type}/{name}/{value}", handlers.UpdateMetricHandler(storage))
