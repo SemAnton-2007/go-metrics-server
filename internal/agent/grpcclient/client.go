10386f1eb1e1a9c32dbcb3eaa480c07d8cda2766
SemAnton-2007 <anton@semenov.su>
2025-08-11 17:23:37 +0300

    Инкремент 25: Поправил тесты

--- internal/agent/grpcclient/client.go
+++ internal/agent/grpcclient/client.go
@@ -18,11 +18,14 @@ type GRPCClient struct {
 }
 
 func New(cfg *config.Config) *GRPCClient {
-	conn, err := grpc.Dial(
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	conn, err := grpc.DialContext(
+		ctx,
 		cfg.GRPCAddress,
 		grpc.WithTransportCredentials(insecure.NewCredentials()),
 		grpc.WithBlock(),
-		grpc.WithTimeout(5*time.Second),
 	)
 	if err != nil {
 		log.Printf("Failed to connect to gRPC server: %v", err)
